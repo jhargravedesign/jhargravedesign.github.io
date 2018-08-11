682c065350b372417191232bdaeaa2141e248abc
jhargravedesign <41926230+jhargravedesign@users.noreply.github.com>
2018-08-11 15:16:12 +0100

    Add files via upload

---
+++ js/gridmain.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){  
+    
+    $('#p-jump-b').click(function () {
+        $('#p-jump-l').slideToggle(200);
+        $('#p-jump-b').toggleClass('p-jump-active circle-0');
+        $('.circle-1').toggle(0);
+    });
+    
+    $('#m-nav').click(function() {
+        $('.m-nav-l').slideToggle(200);
+    });
+    
+    $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+        
+    });
+    
+    $('#p-jump-l > li > a').click(function() {
+        $('#p-jump-l').slideToggle(200);
+        $('#p-jump-b').toggleClass('p-jump-active circle-0');
+        $('.circle-1').toggle(0);
+    });
+});
